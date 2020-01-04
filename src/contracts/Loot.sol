8bbfe88981773a6a6ab6a149a19a75196715faf9
robbie <robbiekruszynski@gmail.com>
2020-01-03 17:48:14 -0800

    add more tests along with logic in the Loot.sol

--- src/contracts/Loot.sol
+++ src/contracts/Loot.sol
@@ -3,7 +3,22 @@ pragma solidity  ^0.5.0;
 import "./ERC721Full.sol";
 
 contract Loot is ERC721Full {
+    string[] public items;
+    mapping(string => bool) _itemExists;
     constructor() ERC721Full("item", "ITEM") public {
 
+     }
+
+//change this to have aminter role opposed to public later on
+     function mint (string memory _item) public {
+         //require unique
+
+         require(!_itemExists[_item]);
+         uint _id = items.push(_item);
+         _mint(msg.sender, _id);
+         _itemExists[_item] = true;
+         // Item - add
+         //call the mint function 
+         //tack it 
      }
 }
--- test/Loot.test.js
+++ test/Loot.test.js
@@ -31,4 +31,23 @@ contract("Loot", accounts => {
       assert.equal(symbol, "ITEM");
     });
   });
+
+  describe("minting", async () => {
+    it("creates a new token", async () => {
+      const result = await contract.mint("EC058E");
+      const totalSupply = await contract.totalSupply();
+      //SUCCESS
+      assert.equal(totalSupply, 1);
+      console.log(result);
+      const event = result.logs[0].args;
+      assert.equal(event.tokenId.toNumber(), 1, "id is correct");
+      assert.equal(
+        event.from,
+        "0x0000000000000000000000000000000000000000",
+        "from is correct"
+      );
+
+      assert.equal(event.to, accounts[0], "to is corrrect");
+    });
+  });
 });

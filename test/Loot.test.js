c4034c917281017cc3065f7796317893ec07ce0f
robbie <robbiekruszynski@gmail.com>
2020-01-03 17:00:59 -0800

    right successful test for address fetch in test DIR

--- test/Loot.test.js
+++ test/Loot.test.js
@@ -1,13 +1,17 @@
-const Loot = artifacts.require("Loot.sol")
+const Loot = artifacts.require("Loot.sol");
 
 require("chai")
-.use(require("chai-as-promised"))
-.should()
+  .use(require("chai-as-promised"))
+  .should();
 
-contract("Loot", (accounts) => {
-    describe("deployment", async() => {
-        it("deploys successfully", async() => {
+contract("Loot", accounts => {
+  let contract;
 
-        }
-    })
-})
+  describe("deployment", async () => {
+    it("deploys successfully", async () => {
+      contract = await Loot.deployed();
+      const address = contract.address;
+      assert.notEqual(address, " ");
+    });
+  });
+});
